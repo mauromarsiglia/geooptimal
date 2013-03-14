fc40533555f2c1c94279d3d237090c2c8ff1d9a0
ACPS <acps91@gmail.com>
2013-03-14 17:15:50 -0500

    update map

--- Geo Optimal/src/com/test/geo/optimal/Main.java
+++ Geo Optimal/src/com/test/geo/optimal/Main.java
@@ -1,15 +1,27 @@
 package com.test.geo.optimal;
 
+
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
-public class Main extends Activity {
 
+public class Main extends MapActivity{
+
+	private MapView mapa = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+
+        mapa = (MapView)findViewById(R.id.mapa);
+ 
+
+        mapa.setBuiltInZoomControls(true);
+		
 	}
 
 	@Override
@@ -17,6 +29,12 @@ public class Main extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 }
